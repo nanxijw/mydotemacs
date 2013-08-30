9cde4f900434d74702bb78e21f929325ad1078d9
nanxijw <nanxijw@gmail.com>
2013-08-30 20:15:43 +0800

    更新

--- .emacs.d/config/misc.el
+++ .emacs.d/config/misc.el
@@ -17,6 +17,19 @@
 (if (fboundp 'fringe-mode)
     (fringe-mode 4))
 
+;; 光标靠近鼠标指针时，让鼠标指针自动让开，别挡住视线。
+(mouse-avoidance-mode 'animate)
+
+;; 可以递归的使用 minibuffer。
+(setq enable-recursive-minibuffers t)
+
+;; 设置 sentence-end 可以识别中文标点。不用在 fill 时在句号后插 入两个空格。
+(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
+(setq sentence-end-double-space nil)
+
+;; 用一个很大的 kill ring. 防止不小心删掉重要的东西。
+(setq kill-ring-max 200)
+
 ;;use file path to ensure buffer name uniqueness
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
@@ -27,9 +40,5 @@
 (require 'highlight-symbol)
 ;; color-moccur
 (require 'color-moccur)
-;; ;; helm
-;; (add-pack-lib "helm")
-;; (require 'helm-config)
-;;;;太慢，屏蔽之 (helm-mode 1)
 
 (load-theme 'solarized-dark t)
