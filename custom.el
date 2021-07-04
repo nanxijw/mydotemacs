3d71c47dbaad74add79e016183ace9163c4be686
nanxijw <nanxijw@sina.com>
2021-07-04 15:25:31 +0800

    update

--- custom.el
+++ custom.el
@@ -4,14 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
+   '("549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" "824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
  '(package-selected-packages
-   (quote
-    (company helm-gtags helm-ag helm projectile symbol-overlay counsel pike-mode use-package solarized-theme highlight-symbol diminish))))
+   '(multiple-cursors rust-mode consult selectrum yasnippet company projectile symbol-overlay counsel pike-mode use-package solarized-theme highlight-symbol diminish)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "等距更纱黑体 SC" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
- '(selectrum-current-candidate ((t (:inherit highlight)))))
+ )
