d2a34431e62b26c94cdcb79d8022cede4c7c2842
nanxijw <nanxijw@sina.com>
2021-07-04 09:25:41 +0800

    update

--- custom.el
+++ custom.el
@@ -4,8 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default)))
+   '("824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
  '(package-selected-packages
    (quote
     (company helm-gtags helm-ag helm projectile symbol-overlay counsel pike-mode use-package solarized-theme highlight-symbol diminish))))
@@ -14,4 +13,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "等距更纱黑体 SC" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
+ '(selectrum-current-candidate ((t (:inherit highlight)))))
