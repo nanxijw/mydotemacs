6ee48dcf6b4c01e349972436b7343f267a41f06e
nanxijw <nanxijw@sina.com>
2021-09-23 15:23:20 +0800

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,13 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" "824d07981667fd7d63488756b6d6a4036bae972d26337babf7b56df6e42f2bcd" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
+   '("549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" "ae4aa4bf7418af9a2a8a0e9d172895a2f25fe725790fed3f259bba53159a8264" "c5a81a42df109b02a9a68dfe0ed530080372c1a0bbcb374da77ee3a57e1be719" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
  '(package-selected-packages
-   '(multiple-cursors rust-mode consult selectrum yasnippet company projectile symbol-overlay counsel pike-mode use-package solarized-theme highlight-symbol diminish)))
+   '(doom-modeline company-posframe citre tree-sitter-langs tree-sitter cnfonts hydra easy-kill orderless multiple-cursors yasnippet ggtags consult dracula-theme rust-mode lsp-mode htmlize meow selectrum s cl-lib company projectile symbol-overlay use-package diminish)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "等距更纱黑体 SC" :foundry "outline" :slant normal :weight normal :height 120 :width normal))))
- '(selectrum-current-candidate ((t (:inherit highlight)))))
+ )
+
+(put 'narrow-to-region 'disabled nil)
