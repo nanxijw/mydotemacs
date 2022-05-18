61aef2883c76a248ab03798b35579599aefdcb46
nanxijw <nanxijw@sina.com>
2022-05-18 20:07:06 +0800

    use submodules to manage three party package

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("549ccbd11c125a4e671a1e8d3609063a91228e918ffb269e57bd2cd2c0a6f1c6" "ae4aa4bf7418af9a2a8a0e9d172895a2f25fe725790fed3f259bba53159a8264" "c5a81a42df109b02a9a68dfe0ed530080372c1a0bbcb374da77ee3a57e1be719" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" default))
  '(package-selected-packages
-   '(cape yasnippet-snippets setup citre vertico-posframe gcmh corfu vertico rustic srcery-theme cnfonts orderless yasnippet consult cl-lib projectile symbol-overlay)))
+   '(cape setup citre vertico-posframe gcmh corfu vertico srcery-theme cnfonts orderless yasnippet consult projectile symbol-overlay)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
