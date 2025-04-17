c1a48c36a4abb0fc28e53b00d7566b62558b2441
nanxijw <nanxijw@sina.com>
2025-04-17 18:08:33 +0800

    改用git submodule管理插件

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("cee5c56dc8b95b345bfe1c88d82d48f89e0f23008b0c2154ef452b2ce348da37" default))
- '(package-selected-packages
-   '(ef-themes corfu vertico yasnippet consult symbol-overlay)))
+   '("cee5c56dc8b95b345bfe1c88d82d48f89e0f23008b0c2154ef452b2ce348da37"
+     default))
+ '(package-selected-packages '(ef-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
